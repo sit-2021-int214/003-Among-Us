0e2f971f8ff9c45ac6b488d23e3c7882f68cd6b6
malixch12 <68807095+malixch12@users.noreply.github.com>
2021-08-25 14:50:32 +0700

    Add files via upload

---
+++ assignment/HW01_63130500018.R
@@ -0,0 +1,38 @@
+#Ex1
+a <- c(10.4,5.6,3.1,6.4,21.7)
+
+sum(a) #47.2
+mean(a) #9.44
+sd(a) #7.33846
+var(a) #53.853
+
+
+#Ex2
+# List of Marvel movies (Order by Marvel Phase released)
+names <- c("Iron Man","The Incredible Hulk","Iron Man 2","Thor","Captain America: The First Avenger",
+           "The Avengers","Iron Man 3","Thor: The Dark World","Captain America: The Winter Soldier",
+           "Guardians of the Galaxy","Avengers: Age of Ultron","Ant-Man","Captain America: Civil War",
+           "Doctor Strange","Guardians of the Galaxy 2","Spider-Man: Homecoming","Thor: Ragnarok","Black Panther",
+           "Avengers: Infinity War","Ant-Man and the Wasp","Captain Marvel","Avengers: Endgame",
+           "Spider-Man: Far From Home","WandaVision","Falcon and the Winter Soldier","Loki","Black Widow")
+
+# List of released year of Marvel movies
+years <- c(2008,2008,2010,2011,2011,2012,2013,2013,2014,2014,2015,2015,2016,2016,
+           2017,2017,2017,2017,2018,2018,2019,2019,2019,2021,2021,2021,2021)
+
+# Or using Function
+years <- c(2008,2008,2010,2011,2011,2012,rep(2013:2016,each=2),
+           rep(2017,4),rep(2018,2),rep(2019,3),rep(2021,4))
+
+length(names) #27
+
+names[19] #"Avengers: Infinity War"
+
+table(years)
+#years
+#2008 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 2021 
+#2    1    2    1    2    2    2    2    4    2    3    4 
+
+
+marvel_movies <- data.frame(names, years)
+
