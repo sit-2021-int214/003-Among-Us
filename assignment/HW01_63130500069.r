ffe9e871dd3f39b58a3311d1dc536b3c9ac3a5db
salmonindaeyo <68809531+salmonindaeyo@users.noreply.github.com>
2021-08-25 14:51:12 +0700

    Add files via upload

---
+++ assignment/HW01_63130500069.r
@@ -0,0 +1,30 @@
+
+#Exercise2
+# List of Marvel movies (Order by Marvel Phase released)
+names <- c("Iron Man","The Incredible Hulk","Iron Man 2","Thor","Captain America: The First Avenger",
+           "The Avengers","Iron Man 3","Thor: The Dark World","Captain America: The Winter Soldier",
+           "Guardians of the Galaxy","Avengers: Age of Ultron","Ant-Man","Captain America: Civil War",
+           "Doctor Strange","Guardians of the Galaxy 2","Spider-Man: Homecoming","Thor: Ragnarok","Black Panther",
+           "Avengers: Infinity War","Ant-Man and the Wasp","Captain Marvel","Avengers: Endgame",
+           "Spider-Man: Far From Home","WandaVision","Falcon and the Winter Soldier","Loki","Black Widow")
+
+# List of released year of Marvel movies
+years <- c(2008,2008,2010,2011,2011,2012,2013,2013,2014,2014,2015,2015,2016,2016,
+           2017,2017,2017,2017,2018,2018,2019,2019,2019,2021,2021,2021,2021)
+
+# Or using Function
+years <- c(2008,2008,2010,2011,2011,2012,rep(2013:2016,each=2),
+           rep(2017,4),rep(2018,2),rep(2019,3),rep(2021,4))
+
+#dataframe_marvel
+marvel_movies <- data.frame(names,years)
+marvel_movies
+
+#The numbers of movies
+length(marvel_movies)
+
+#19th_movies
+marvel_movies[19]
+
+#most released movies ans : 2018 , 2021
+table(years)
